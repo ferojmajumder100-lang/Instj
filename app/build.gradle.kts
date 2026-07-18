196882345074c1a305d0712f8a5fc6ad9abb8030
ferojmajumder100-lang <ferojmajumder100@gmail.com>
2026-07-18 11:42:40 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
       isMinifyEnabled = true
       isShrinkResources = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
